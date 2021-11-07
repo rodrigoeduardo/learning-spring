24856c53fcb0e1f8ae54cbdcbf9ab2fb7b413ff7
Rodrigo Eduardo <rodrigoow99@hotmail.com>
2021-11-07 03:31:43 -0300

    created my first controller

---
+++ src/main/java/com/example/springintro/firstController.java
@@ -0,0 +1,15 @@
+package com.example.springintro;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class firstController {
+
+  @GetMapping("/hello")
+  @ResponseBody
+  public String hello() {
+    return "Hello";
+  }
+}
